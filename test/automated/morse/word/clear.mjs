2809ff7c7a214b9b69c40afc0043ec7e95b6bfd8
Nick Arora <arora.nick@gmail.com>
2025-04-12 22:28:29 -0700

    Tests are reorganized

--- test/automated/morse/word/clear.mjs
+++ test/automated/morse/word/clear.mjs
@@ -2,20 +2,18 @@ import { assert } from "#test/automated/test-init"
 
 import { Word, Character } from "#controls"
 
-describe("Morse", () => {
-  describe("Word", () => {
-    describe("Clear", () => {
-      const character = Character.example()
+describe("Word", () => {
+  describe("Clear", () => {
+    const character = Character.example()
 
-      const word = Word.example([character])
+    const word = Word.example([character])
 
-      word.clear()
+    word.clear()
 
-      const characters = word.characters()
+    const characters = word.characters()
 
-      it("Cleared", () => {
-        assert.deepStrictEqual(characters, [])
-      })
+    it("Cleared", () => {
+      assert.deepStrictEqual(characters, [])
     })
   })
 })
