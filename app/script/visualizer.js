dd7d8b651ead4025c44feb5194e645311eac910b
Nick Arora <arora.nick@gmail.com>
2025-04-16 17:59:59 -0700

    Enable root element when character is in progress

--- app/script/visualizer.js
+++ app/script/visualizer.js
@@ -1,21 +1,36 @@
 const morseEnabledClass = "on"
 
-function enableCharacter(decodedCharacter) {
-  const elementID = `morse-element-${decodedCharacter.toLowerCase()}`
+function morseElementID(character) {
+  return `morse-element-${character.toLowerCase()}`
+}
 
-  const element = document.getElementById(elementID)
-
-  if (!element || element.classList.contains(morseEnabledClass)) {
+function addClass(element, className) {
+  if (!element || element.classList.contains(className)) {
     return
   }
 
-  element.classList.add(morseEnabledClass)
+  element.classList.add(className)
+}
+
+function removeClass(element, className) {
+  element.classList.remove(className)
+}
+
+function enableCharacter(decodedCharacter) {
+  const rootElementID = morseElementID("root")
+  const rootElement = document.getElementById(rootElementID)
+
+  const elementID = morseElementID(decodedCharacter)
+  const element = document.getElementById(elementID)
+
+  addClass(rootElement, morseEnabledClass)
+  addClass(element, morseEnabledClass)
 }
 
 function disableCharacters() {
   const elements = document.querySelectorAll('[id^="morse-element-"]')
 
   for (element of elements) {
-    element.classList.remove(morseEnabledClass)
+    removeClass(element, morseEnabledClass)
   }
 }
